0370605553774f4822338c271740cf20245d3586
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-06 09:30:34 +0530

    Added:Insert b/w two node

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,9 @@
 package com.practiceProgram;
 
-public class LinkedListUC3 {
-	public Node head = null;
+public class InsertUC4 {
+	public static  Node head = null;
 	public Node tail = null;
-	
+
 	class Node {
 		int data;
 		Node next;
@@ -28,43 +28,60 @@ public class LinkedListUC3 {
 		public void setNext(Node next) {
 			this.next = next;
 		}
-
 	}
 
-	
-//	public void addNode(int data) {
-//		Node newNode= new Node (data);
-//
-//		if(head == null) {
-//			head = newNode;
-//			tail = newNode;
-//		}
-//		else {
-//			tail.next = newNode;
-//			tail = newNode;
-//			
-//		}
-//	}
-		
-
-
-	public void appened (int data) {
+	public void addNode(int data) {
 		Node newNode= new Node (data);
-		if (this.head == null ||this.tail == null ) {
-			this.head = newNode;
-			this.tail =newNode;
+
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
 		}
-//		if (this.tail == null) {
-//			this.tail =newNode;
-//		}
 		else {
-			this.tail.setNext(newNode);
-			this.tail = newNode;
-			
+			tail.next = newNode;
+			tail = newNode;
+
 		}
 	}
-	
-	
+
+
+	//	public void insert(int data, newNode) {
+	//		
+	//		Node newNode= new Node (data);
+	//		Node tempNode = newNode.getNext();
+	//        newNode.setNext(newNode);
+	//		newNode.setNext(tempNode);
+	//	}
+	//
+	//    Node newNode(int data)
+	//    {
+	//        Node x = new Node(data);
+	//        return x;
+	//    
+
+	void insertMid(int data) {
+		if (head == null)
+			head = new Node(data);
+		else {
+			Node newNode = new Node (data);
+			Node newhead = head;
+			int len = 0;
+			while (newhead != null) {
+				len++;
+				newhead =newhead.next;
+			}
+			int count = ((len % 2) == 0) ? (len / 2) :
+				(len + 1) / 2;
+			newhead = head;
+
+			while (count > 1)
+				newhead = newhead.next;
+
+			newNode.next = newhead.next;
+			newhead.next = newNode;
+		}
+	}
+
 	public void display() {
 		Node tempNode = head;
 		Node current = head;
@@ -72,22 +89,25 @@ public class LinkedListUC3 {
 			System.out.println("List is Empty");
 			return;
 		}
-		System.out.println("Singly LinkedList of Node");
+		//		System.out.println("Singly LinkedList of Node");
 		while(current != null) {
 			System.out.print(current.data + " ");
 			current = current.next;
 		}
 		System.out.println();
-		}
-	
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LinkedListUC3 list = new LinkedListUC3();
-		
-		list.appened(56);
-		list.appened(30);
-		list.appened(70);
-	
+		InsertUC4 list = new InsertUC4();
+
+		list.addNode(56);
+		list.addNode(70);
+
+		list.display();
+
+		System.out.println("After inset data in mid");
+		list.insertMid(30);
 		list.display();
 	}
 }

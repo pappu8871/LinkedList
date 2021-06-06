ef2cd7a4277a98486fa2881504c0c82a1185cf0e
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-06 09:19:32 +0530

    Added:LinkedListUC2.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,9 @@
 package com.practiceProgram;
 
-public class LinkedListUC1 {
-
+public class LinkedListUC2 {
 	public Node head = null;
 	public Node tail = null;
-	
+
 	class Node {
 		int data;
 		Node next;
@@ -13,22 +12,54 @@ public class LinkedListUC1 {
 			this.data = data;
 			this.next = null;
 		}
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
 	}
+
 
 	public void addNode(int data) {
 		Node newNode= new Node (data);
+		//
+		//		if(head == null) {
+		//			head = newNode;
+		//			tail = newNode;
+		//		}
+		//		else {
+		//			tail.next = newNode;
+		//			tail = newNode;
+		//			
+		//		}
 
-		if(head == null) {
-			head = newNode;
-			tail = newNode;
+		if(this.tail == null) {
+			this.tail = newNode;
 		}
-		else {
-			tail.next = newNode;
-			tail = newNode;
+		if(this.head == null) {
+			this.head = newNode;
+
+		} else {
+			Node tempNode = this.head;
+			this.head =newNode;
+			this.head.setNext(tempNode);
 		}
+
 	}
 
 	public void display() {
+		Node tempNode = head;
 		Node current = head;
 		if(head == null) {
 			System.out.println("List is Empty");
@@ -36,18 +67,22 @@ public class LinkedListUC1 {
 		}
 		System.out.println("Singly LinkedList of Node");
 		while(current != null) {
-			System.out.print(current.data + " " );
+			System.out.print(current.data + " ");
 			current = current.next;
 		}
+		System.out.println();
 	}
 
-	public static void main(String[] args){
-		LinkedListUC1 list = new LinkedListUC1();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedListUC2 list = new LinkedListUC2();
 
-		list.addNode(56);
-		list.addNode(30);
 		list.addNode(70);
+		list.addNode(30);
+		list.addNode(56);
 
 		list.display();
-	} 
+	}
 }
+
+

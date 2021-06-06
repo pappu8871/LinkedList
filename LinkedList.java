5ab367d81d052460648607405a5701106134a467
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-06 09:33:45 +0530

    Added:Delete First Node

--- LinkedList.java
+++ LinkedList.java
@@ -1,13 +1,14 @@
 package com.practiceProgram;
 
-public class InsertUC4 {
-	public static  Node head = null;
+public class DeletFirstUC5 {
+
+	public static Node head = null;
 	public Node tail = null;
 
 	class Node {
 		int data;
 		Node next;
-		
+
 		public Node(int data) {
 			this.data = data;
 			this.next = null;
@@ -28,7 +29,9 @@ public class InsertUC4 {
 		public void setNext(Node next) {
 			this.next = next;
 		}
+
 	}
+
 
 	public void addNode(int data) {
 		Node newNode= new Node (data);
@@ -44,43 +47,30 @@ public class InsertUC4 {
 		}
 	}
 
-
-	//	public void insert(int data, newNode) {
-	//		
-	//		Node newNode= new Node (data);
-	//		Node tempNode = newNode.getNext();
-	//        newNode.setNext(newNode);
-	//		newNode.setNext(tempNode);
-	//	}
-	//
-	//    Node newNode(int data)
-	//    {
-	//        Node x = new Node(data);
-	//        return x;
-	//    
-
-	void insertMid(int data) {
+	public Node DeletFirstNode() {
 		if (head == null)
-			head = new Node(data);
-		else {
-			Node newNode = new Node (data);
-			Node newhead = head;
-			int len = 0;
-			while (newhead != null) {
-				len++;
-				newhead =newhead.next;
-			}
-			int count = ((len % 2) == 0) ? (len / 2) :
-				(len + 1) / 2;
-			newhead = head;
-
-			while (count > 1)
-				newhead = newhead.next;
-
-			newNode.next = newhead.next;
-			newhead.next = newNode;
-		}
+			return head;
+		Node temp = head ;
+		head = head.next;
+		return head;
 	}
+
+	//	public void appened (int data) {
+	//		Node newNode= new Node (data);
+	//		if (this.head == null ||this.tail == null ) {
+	//			this.head = newNode;
+	//			this.tail =newNode;
+	//		}
+	////		if (this.tail == null) {
+	////			this.tail =newNode;
+	////		}
+	//		else {
+	//			this.tail.setNext(newNode);
+	//			this.tail = newNode;
+	//			
+	//		}
+	//	}
+
 
 	public void display() {
 		Node tempNode = head;
@@ -89,7 +79,7 @@ public class InsertUC4 {
 			System.out.println("List is Empty");
 			return;
 		}
-		//		System.out.println("Singly LinkedList of Node");
+		System.out.println("Singly LinkedList of Node");
 		while(current != null) {
 			System.out.print(current.data + " ");
 			current = current.next;
@@ -99,15 +89,18 @@ public class InsertUC4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		InsertUC4 list = new InsertUC4();
+		DeletFirstUC5 list = new DeletFirstUC5();
 
 		list.addNode(56);
+		list.addNode(30);
 		list.addNode(70);
 
 		list.display();
 
-		System.out.println("After inset data in mid");
-		list.insertMid(30);
+		list.DeletFirstNode();
+	
+		System.out.println("After Delete of the list");
 		list.display();
 	}
+
 }
